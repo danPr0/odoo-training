e52d87597afdec42dd499062a6aff77c0b6596d5
Dan_Pro <danpr080704@gmail.com>
2023-11-28 13:10:26 +0200

    14 chapter

--- estate_account/models/estate_property.py
+++ estate_account/models/estate_property.py
@@ -1,13 +1,29 @@
-from odoo import models
+from odoo import models, Command
 
 
 class EstateProperty(models.Model):
     _inherit = 'estate.property'
 
     def sell_property(self):
-        # self.env['account.move'].create({'partner_id': self.salesperson_id, 'move_type': 'out_invoice'})
         if super().sell_property():
-            pass
+            for record in self:
+                self.env['account.move'].create(
+                    {
+                        'partner_id': record.buyer_id.id,
+                        'move_type': 'out_invoice',
+                        'invoice_line_ids': [
+                            Command.create({
+                                'name': '6% of the selling price',
+                                'quantity': 1.0,
+                                'price_unit': 0.06 * record.selling_price
+                            }),
+                            Command.create({
+                                'name': 'An additional 100.00 from administrative fees',
+                                'quantity': 1.0,
+                                'price_unit': 100.00
+                            })
+                        ]
+                    })
             return True
         else:
             return False
